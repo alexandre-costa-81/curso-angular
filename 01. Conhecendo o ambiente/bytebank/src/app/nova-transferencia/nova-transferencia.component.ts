bd41f01d0a508e31399530e84dcf30bd14945de8
Alexandre Costa <alexandre.costa@inf.ufpel.edu.br>
2021-09-14 20:37:45 -0300

    Interpolando dados

--- 01. Conhecendo o ambiente/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ 01. Conhecendo o ambiente/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -13,7 +13,15 @@ export class NovaTransferenciaComponent {
 
   transferir() {
     console.log('Solicitada nova transferência');
+
     const valorEmitir = {valor: this.valor, destino: this.destino};
     this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = undefined;
+    this.destino = undefined;
   }
 }
