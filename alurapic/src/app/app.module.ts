6f343ce77960f797e1ebe5685ff0bbd125722c1a
Alexandre Costa <alexandre.costa@inf.ufpel.edu.br>
2021-09-15 00:55:31 -0300

    Organizando nosso código em módulos

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PhotoComponent } from './photo/photo.component';
-
+import { PhotosModule } from './photos/photos.module';
 @NgModule({
-  declarations: [	
-    AppComponent,
-      PhotoComponent
+  declarations: [
+    AppComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
