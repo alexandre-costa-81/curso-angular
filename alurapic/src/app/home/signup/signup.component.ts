63d9c3bf89802d58a124638226dd10499cc81b8d
Alexandre Costa <alexandre.costa@inf.ufpel.edu.br>
2021-09-16 21:50:38 -0300

    Validação do formulário

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,6 +1,52 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './signup.component.html'
 })
-export class SignUpComponent {}
+export class SignUpComponent implements OnInit {
+
+  signupForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.signupForm = new FormGroup({
+      email: new FormControl(''),
+      fullName: new FormControl(''),
+      userName: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      fullName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]
+      ],
+      userName: ['',
+        [
+          Validators.required,
+          Validators.pattern(/^[a-z0-9_\-]+$/),
+          Validators.minLength(2),
+          Validators.maxLength(30)
+        ]
+      ],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14)
+        ]
+      ]
+    });
+  }
+}
