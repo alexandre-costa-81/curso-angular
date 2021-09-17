938e0be614721c1fe90925c5d1f11d67fbb0f791
Alexandre Costa <alexandre.costa@inf.ufpel.edu.br>
2021-09-16 21:04:19 -0300

    logout

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
@@ -9,7 +10,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule
   ],
   exports: [HeaderComponent]
 })
